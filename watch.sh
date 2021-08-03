caf3abf4f25fe52c7803d3f135828826724b0ae4
Augustin Ribreau <augustin.ribreau@gmail.com>
2021-08-03 14:28:41 +0200

    fix: start script and ignore node_modules

--- watch.sh
+++ watch.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # dependency: npm i nodemon -g
 
-nodemon -w src/ -e "css,properties,json,js,ftl" --exec "bash ./build.sh"
+nodemon -w src/ -e "css,properties,json,js,ftl,scss" --exec "bash ./build-and-restart-keycloak.sh"
